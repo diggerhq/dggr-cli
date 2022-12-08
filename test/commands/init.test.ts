f81e52ad37064a6fd6e08925643db0afbeafa694
Matas Jonys <matas@digger.dev>
2022-12-08 14:58:44 +0000

    Update test

--- test/commands/init.test.ts
+++ test/commands/init.test.ts
@@ -5,13 +5,6 @@ describe("init", () => {
     .stdout()
     .command(["init"])
     .it("runs hello", (ctx) => {
-      expect(ctx.stdout).to.contain("hello world");
-    });
-
-  test
-    .stdout()
-    .command(["init", "--name", "jeff"])
-    .it("runs hello --name jeff", (ctx) => {
-      expect(ctx.stdout).to.contain("hello jeff");
+      expect(ctx.stdout).to.contain("Successfully initiated a Digger project");
     });
 });
