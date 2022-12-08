a6e980a07ddafcbba572de7d76d62decfcdfb3fb
Matas J <matasjonys@gmail.com>
2022-12-08 14:50:46 +0000

    Add init command (#2)
    * Add init command
    
    * Remove hello world tests
    
    Co-authored-by: Matas Jonys <matas@digger.dev>

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,4 +1,5 @@
 import { Command, Flags } from "@oclif/core";
+import * as fs from "fs";
 
 export default class Init extends Command {
   static description = "describe the command here";
@@ -6,23 +7,34 @@ export default class Init extends Command {
   static examples = ["<%= config.bin %> <%= command.id %>"];
 
   static flags = {
-    // flag with a value (-n, --name=VALUE)
-    name: Flags.string({ char: "n", description: "name to print" }),
     // flag with no value (-f, --force)
     force: Flags.boolean({ char: "f" }),
   };
 
-  static args = [{ name: "file" }];
-
   public async run(): Promise<void> {
     const { args, flags } = await this.parse(Init);
+    const { root, version } = this.config;
+    const diggerJson = `${root}/digger.json`;
 
-    const name = flags.name ?? "world";
-    this.log(
-      `hello ${name} from /Users/matasjonys/Projects/dggr-cli/src/commands/init.ts`
-    );
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`);
+    try {
+      if (fs.existsSync(diggerJson) && !flags.force) {
+        const rawContent = fs.readFileSync(diggerJson, "utf-8");
+        const parsedContent = JSON.parse(rawContent);
+        fs.writeFileSync(
+          diggerJson,
+          JSON.stringify({ ...parsedContent, updated: Date.now() })
+        );
+        this.log("Successfully updated a Digger project");
+      } else {
+        const content = {
+          version,
+          created: Date.now(),
+        };
+        fs.writeFileSync(diggerJson, JSON.stringify(content));
+        this.log("Successfully initiated a Digger project");
+      }
+    } catch (err: any) {
+      this.error(err);
     }
   }
 }
