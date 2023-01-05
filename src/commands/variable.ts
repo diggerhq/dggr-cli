0b1e471abd20031ec057699baeae098700ee2797
Matas J <matasjonys@gmail.com>
2023-01-05 16:15:17 +0000

    Variable add (#51)
    * WIP Variable
    
    * Enable variable addition
    
    Co-authored-by: Matas Jonys <matas@digger.dev>

--- src/commands/variable.ts
+++ src/commands/variable.ts
@@ -1,27 +1,94 @@
 import { Flags } from "@oclif/core";
 import { BaseCommand } from "./base";
+import { ConfigIniParser } from "config-ini-parser";
+import fs from "node:fs";
 
 export default class Variable extends BaseCommand<typeof Variable> {
-  static description = "describe the command here";
+  static description = "Manage environment variables for your infrastructure";
 
   static examples = ["<%= config.bin %> <%= command.id %>"];
 
   static flags = {
-    // flag with a value (-n, --name=VALUE)
-    name: Flags.string({ char: "n", description: "name to print" }),
-    // flag with no value (-f, --force)
-    force: Flags.boolean({ char: "f" }),
+    block: Flags.string({ char: "b", description: "name of the block" }),
   };
 
-  static args = [{ name: "file" }];
+  static args = [
+    {
+      name: "command",
+      options: ["add", "get", "delete", "import", "list"],
+    },
+    { name: "kv" },
+  ];
 
   public async run(): Promise<void> {
     const { args, flags } = await this.parse(Variable);
+    const parser = new ConfigIniParser();
+    const iniFile = fs.readFileSync(
+      `${process.cwd()}/dgctl.variables.ini`,
+      "utf8"
+    );
+    parser.parse(iniFile);
 
-    const name = flags.name ?? "world";
-    this.log(`hello ${name}`);
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`);
+    if (!args.command) {
+      this.log(
+        "No command provided. Example command: dgctl variable <add> -b=myapp key=value"
+      );
+      return;
+    }
+
+    switch (args.command) {
+      case "add": {
+        if (!flags.block) {
+          this.log(
+            "No block provided for the variable. Example command: dgctl variable add -b=<myapp> key=value"
+          );
+          return;
+        }
+
+        if (!args.kv) {
+          this.log(
+            "No key or value provided. Example command: dgctl variable add -b=myapp <key=value>"
+          );
+          return;
+        }
+
+        if (!parser.isHaveSection(flags.block)) {
+          parser.addSection(flags.block);
+        }
+
+        const [key, value] = args.kv.split("=");
+        parser.set(flags.block, key, value);
+
+
+        fs.writeFileSync(
+          `${process.cwd()}/dgctl.variables.ini`,
+          parser.stringify("\n")
+        );
+
+        return;
+      }
+
+      case "get": {
+        this.log(args.kv);
+
+        return;
+      }
+
+      case "delete": {
+        this.log(args.kv);
+
+        return;
+      }
+
+      case "import": {
+        this.log(args.kv);
+
+        return;
+      }
+
+      case "list": {
+        this.log(args.kv);
+      }
     }
   }
 }
