45375e4640d8c702fa90a3274e93eaa34307da83
Dias Saparov <dias@digger.dev>
2023-01-05 11:58:12 +0000

    introduce subcommands instead of big switch statement in the block command

---
+++ src/commands/block/add.ts
@@ -0,0 +1,47 @@
+import {Command, Flags} from '@oclif/core'
+import { createBlock } from '../../utils/helpers'
+import { trackEvent } from '../../utils/mixpanel'
+
+export default class Add extends Command {
+  static description = 'Adds a block to the project'
+
+  static flags = {
+    type: Flags.string({
+      char: "t",
+      description: "type of block",
+      options: ["container", "mysql", "postgres", "docdb", "redis"],
+    }),
+    name: Flags.string({
+      char: "n",
+      description: "new name for the block",
+    }),
+  }
+
+  static args = [{name: 'name', description: "new name for the block"}]
+
+  public async run(): Promise<void> {
+    const {args, flags} = await this.parse(Add)
+
+    trackEvent("block add called", { flags, args });
+    if (!flags.type) {
+      this.log(
+        "No type provided for the block. Example: dgctl block add -t=container <name>"
+      );
+      return;
+    }
+
+    if (!args.name) {
+      this.log("No name provided, will be using an auto generated name.");
+    }
+
+    const blockName =
+      args.name ?? `${flags.type}_${crypto.randomUUID().slice(0, 5)}`;
+    const type = flags.type;
+    try {
+      createBlock(type, blockName, {});
+      this.log("Successfully added a block to the Digger project");
+    } catch (error: any) {
+      this.error(error);
+    }
+  }
+}
