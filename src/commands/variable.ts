f911df60e4dbdd8efbbd26b3589ea0ad69b9fd96
Matas Jonys <matas@digger.dev>
2023-01-05 13:47:42 +0000

    Enable variable addition

--- src/commands/variable.ts
+++ src/commands/variable.ts
@@ -1,5 +1,7 @@
 import { Flags } from "@oclif/core";
 import { BaseCommand } from "./base";
+import { ConfigIniParser } from "config-ini-parser";
+import fs from "node:fs";
 
 export default class Variable extends BaseCommand<typeof Variable> {
   static description = "Manage environment variables for your infrastructure";
@@ -20,18 +22,48 @@ export default class Variable extends BaseCommand<typeof Variable> {
 
   public async run(): Promise<void> {
     const { args, flags } = await this.parse(Variable);
+    const parser = new ConfigIniParser();
+    const iniFile = fs.readFileSync(
+      `${process.cwd()}/dgctl.variables.ini`,
+      "utf8"
+    );
+    parser.parse(iniFile);
 
     if (!args.command) {
       this.log(
-        "No block provided for the variable. Example command: dgctl variable add -b=myapp key=value"
+        "No command provided. Example command: dgctl variable <add> -b=myapp key=value"
       );
       return;
     }
 
     switch (args.command) {
       case "add": {
-        this.log(flags.block);
-        this.log(args.kv);
+        if (!flags.block) {
+          this.log(
+            "No block provided for the variable. Example command: dgctl variable add -b=<myapp> key=value"
+          );
+          return;
+        }
+
+        if (!args.kv) {
+          this.log(
+            "No key or value provided. Example command: dgctl variable add -b=myapp <key=value>"
+          );
+          return;
+        }
+
+        if (!parser.isHaveSection(flags.block)) {
+          parser.addSection(flags.block);
+        }
+
+        const [key, value] = args.kv.split("=");
+        parser.set(flags.block, key, value);
+
+
+        fs.writeFileSync(
+          `${process.cwd()}/dgctl.variables.ini`,
+          parser.stringify("\n")
+        );
 
         return;
       }
