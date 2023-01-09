3f924af5552cff0443bf086fcc827057cc767ec3
motatoes <moe.habib9@gmail.com>
2023-01-09 18:22:39 +0000

    remove console log

--- src/commands/generate.ts
+++ src/commands/generate.ts
@@ -47,7 +47,7 @@ export default class Generate extends BaseCommand<typeof Generate> {
       diggerConfig: currentDiggerJson,
       combinedJson,
     });
-    console.log(combinedJson)
+
     const response = await axios.post(getTrowelUrl(), combinedJson);
 
     // write response to file
