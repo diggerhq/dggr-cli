b234c413d79b040c274561dab5537d1fab92e455
Dias Saparov <dias@digger.dev>
2023-01-19 11:35:19 +0000

    installation script (#107)
    * installation script
    
    * installation script
    
    * fix urls

---
+++ bin/install.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Check the operating system
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)
+        os="Linux"
+        ;;
+    Darwin*)
+        os="Mac"
+        ;;
+    *)
+        os="Other"
+        ;;
+esac
+
+# Check the architecture
+architectureOut="$(uname -m)"
+case "${architectureOut}" in
+    x86_64*)
+        architecture="x86_64"
+        ;;
+    arm64*)
+        architecture="arm64"
+        ;;
+    arm*)
+        architecture="arm"
+        ;;
+    *)
+        architecture="Other"
+        ;;
+esac
+
+if [ "$os" == "Linux" ] && [ "$architecture" == "x86_64" ]; then
+    url="https://github.com/diggerhq/dggr-cli/releases/latest/download/dgctl-linux-x64.tar.gz"
+elif [ "$os" == "Linux" ] && [ "$architecture" == "arm" ]; then
+    url="https://github.com/diggerhq/dggr-cli/releases/latest/download/dgctl-linux-arm.tar.gz"
+elif [ "$os" == "Mac" ] && [ "$architecture" == "x86_64" ]; then
+    url="https://github.com/diggerhq/dggr-cli/releases/latest/download/dgctl-darwin-x64.tar.gz"
+elif [ "$os" == "Mac" ] && [ "$architecture" == "arm64" ]; then
+    url="https://github.com/diggerhq/dggr-cli/releases/latest/download/dgctl-darwin-arm64.tar.gz"
+elif [ "$os" == "Other" ] || [ "$architecture" == "Other" ] ; then
+    echo "Error: Unsupported platform"
+    exit 1
+elif [ "$os" == "Linux" ]; then
+    url="https://github.com/diggerhq/dggr-cli/releases/latest/download/dgctl-linux-x64.tar.gz"
+elif [ "$os" == "Mac" ]; then
+    url="https://github.com/diggerhq/dggr-cli/releases/latest/download/dgctl-darwin-x64.tar.gz"
+else
+    echo "Error: Unsupported platform"
+    exit 1
+fi
+
+curl -sL $url -o dgctl.tar.gz | tar -xz -C /usr/local
+
+sudo chmod +x /usr/local/dgctl/dgctl
+
+sudo ln -s /usr/local/bin/dgctl /usr/local/dgctl/dgctl
