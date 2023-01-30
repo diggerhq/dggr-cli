6281fa840df1c3dc8a7e434c5cf57a620f22273e
Matas Jonys <matas@digger.dev>
2023-01-30 11:17:03 +0000

    Changes to comply with newer JS standard

--- src/utils/remote-state.ts
+++ src/utils/remote-state.ts
@@ -1,154 +1,131 @@
-import {DynamoDB} from "@aws-sdk/client-dynamodb";
+import { DynamoDB } from "@aws-sdk/client-dynamodb";
 import {
   CreateBucketCommand,
   ListBucketsCommand,
   PutBucketEncryptionCommand,
-  PutBucketVersioningCommand, PutPublicAccessBlockCommand,
+  PutBucketVersioningCommand,
+  PutPublicAccessBlockCommand,
   S3Client,
 } from "@aws-sdk/client-s3";
 
+const ddbClient = new DynamoDB({ region: "us-east-1" });
 
-const ddbClient = new DynamoDB({region: "us-east-1"});
-
-const s3Client = new S3Client({region: "us-east-1"});
+const s3Client = new S3Client({ region: "us-east-1" });
 const createBucket = async (s3Client: S3Client, bucketName: string) => {
   // Create the parameters for calling createBucket
   const bucketParams = {
-    Bucket: bucketName
+    Bucket: bucketName,
   };
 
-  let bucketExists = false;
+  try {
+    const bucketsList = await s3Client.send(new ListBucketsCommand({}));
 
-  const bucketsList = await s3Client.send(new ListBucketsCommand({}));
-  if (bucketsList && bucketsList?.Buckets) {
-    for (const element of bucketsList?.Buckets) {
-      if (element.Name == bucketName) bucketExists = true
+    const bucketExists = bucketsList?.Buckets?.find(
+      (bucket) => bucket.Name === bucketName
+    );
+
+    if (bucketExists) {
+      console.log(`S3 bucket ${bucketName} already exists.`);
+      return;
     }
-  }
 
-
-  if (bucketExists) {
-    console.log(`S3 bucket ${bucketName} already exists.`)
-    return
-  }
-
-  // call S3 to create the bucket
-  try {
+    // call S3 to create the bucket
     await s3Client.send(new CreateBucketCommand(bucketParams));
-  } catch (error) {
-    console.log("Error:", error);
-    return
-  }
 
-  console.log(`Bucket ${bucketName} created successfully.`);
+    console.log(`Bucket ${bucketName} created successfully.`);
 
-  const versioningParams = {
-    Bucket: bucketName,
-    VersioningConfiguration: {
-      MFADelete: 'Disabled',
-      Status: 'Enabled'
-    },
-  };
+    const versioningParams = {
+      Bucket: bucketName,
+      VersioningConfiguration: {
+        MFADelete: "Disabled",
+        Status: "Enabled",
+      },
+    };
 
-  try {
     await s3Client.send(new PutBucketVersioningCommand(versioningParams));
-  } catch (error) {
-    console.log("Error:", error);
-    return
-  }
 
-  console.log(`Versioning enabled for ${bucketName} bucket.`);
+    console.log(`Versioning enabled for ${bucketName} bucket.`);
 
-  const encryptionParams = {
-    Bucket: bucketName,
-    ServerSideEncryptionConfiguration: {
-      Rules: [
-        {
-          ApplyServerSideEncryptionByDefault: {
-            SSEAlgorithm: "aws:kms",
-            //  KMSMasterKeyID: 'STRING_VALUE'
+    const encryptionParams = {
+      Bucket: bucketName,
+      ServerSideEncryptionConfiguration: {
+        Rules: [
+          {
+            ApplyServerSideEncryptionByDefault: {
+              SSEAlgorithm: "aws:kms",
+              //  KMSMasterKeyID: 'STRING_VALUE'
+            },
+            BucketKeyEnabled: true,
           },
-          BucketKeyEnabled: true
-        },
-      ]
-    },
-  };
+        ],
+      },
+    };
 
-  try {
     await s3Client.send(new PutBucketEncryptionCommand(encryptionParams));
-  } catch (error) {
-    console.log("Error:", error);
-    return
-  }
 
-  console.log(`Encryption enabled for ${bucketName} bucket.`);
+    console.log(`Encryption enabled for ${bucketName} bucket.`);
 
-  const publicAccessParams = {
-    Bucket: bucketName,
-    PublicAccessBlockConfiguration: {
-      BlockPublicAcls: true,
-      BlockPublicPolicy: true,
-      IgnorePublicAcls: true,
-      RestrictPublicBuckets: true,
-    },
-  };
+    const publicAccessParams = {
+      Bucket: bucketName,
+      PublicAccessBlockConfiguration: {
+        BlockPublicAcls: true,
+        BlockPublicPolicy: true,
+        IgnorePublicAcls: true,
+        RestrictPublicBuckets: true,
+      },
+    };
 
-  try {
     await s3Client.send(new PutPublicAccessBlockCommand(publicAccessParams));
   } catch (error) {
     console.log("Error:", error);
-    return
+    return;
   }
 
   console.log(`Public access disabled for ${bucketName} bucket.`);
-}
+};
 
 const createDynamoDB = async (ddbClient: DynamoDB, ddbTableName: string) => {
+  const data = await ddbClient.listTables({});
 
-  let ddbTableExists = false;
-
-  const data = await ddbClient.listTables({})
-  if (data && data?.TableNames ) {
-    for (const tableName of data?.TableNames) {
-      if (tableName == ddbTableName) ddbTableExists = true
-    }
-  }
+  const ddbTableExists = data?.TableNames?.find(
+    (table) => table === ddbTableName
+  );
 
   if (ddbTableExists) {
-    console.log(`DynamoDB table ${ddbTableName} already exists.`)
-    return
+    console.log(`DynamoDB table ${ddbTableName} already exists.`);
+    return;
   }
 
   const params = {
     TableName: ddbTableName,
-    KeySchema: [
-      {AttributeName: "LockID", KeyType: "HASH"},
-    ],
-    AttributeDefinitions: [
-      {AttributeName: "LockID", AttributeType: "S"}
-    ],
-    BillingMode: "PAY_PER_REQUEST"
+    KeySchema: [{ AttributeName: "LockID", KeyType: "HASH" }],
+    AttributeDefinitions: [{ AttributeName: "LockID", AttributeType: "S" }],
+    BillingMode: "PAY_PER_REQUEST",
   };
 
-  ddbClient.createTable(params, function (err: any, data: any) {
-    if (err) {
-      if (err.name == "ResourceInUseException") {
-        // table already exist, do nothing
-        console.log(`DynamoDB table ${params.TableName} already exists, skipping creation.`)
-      } else {
-        console.log("Error", err.name);
-      }
+  try {
+    await ddbClient.createTable(params);
+
+    console.log(
+      `DynamoDB table ${params.TableName} has been created successfully.`
+    );
+  } catch (error: any) {
+    if (error.name === "ResourceInUseException") {
+      // table already exist, do nothing
+      console.log(
+        `DynamoDB table ${params.TableName} already exists, skipping creation.`
+      );
     } else {
-      console.log(`DynamoDB table ${params.TableName} has been created successfully.`)
+      console.log("Error", error.name);
     }
-  });
-}
+  }
+};
 
 export const createRemoteState = async (diggerConfig: any) => {
   if (diggerConfig.remote_state !== "local") {
-    const ddbTableName = "digger-terraform-state-lock"
-    const bucketName = 'digger-remote-state-bucket';
-    createBucket(s3Client, bucketName)
-    createDynamoDB(ddbClient, ddbTableName)
+    const ddbTableName = "digger-terraform-state-lock";
+    const bucketName = "digger-remote-state-bucket";
+    await createBucket(s3Client, bucketName);
+    await createDynamoDB(ddbClient, ddbTableName);
   }
 };
